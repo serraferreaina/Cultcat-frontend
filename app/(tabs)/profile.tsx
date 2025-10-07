ca50c0acba70395d09c868eb8c5f31d7cb8a729d
panini09 <albert.panicello.torras@estudiantat.upc.edu>
2025-10-07 17:30:02 +0200

    prova de commit

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native';
 export default function Profile() {
   return <View style={{ flex:1, alignItems:'center', justifyContent:'center' }}>
-    <Text>Perfil de l'usuari</Text>
+    <Text>Perfil de l'usuari/a</Text>
   </View>;
 }
