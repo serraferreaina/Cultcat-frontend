286f95746dfebf4ba8a7d0815b230d7780554f33
PathologicalMiss <toni.gratacos@estudiantat.upc.edu>
2025-10-11 13:43:10 +0200

    feat(language): create i18n.ts file

---
+++ i18n.ts
@@ -0,0 +1,27 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './locales/en/translation.json';
+import es from './locales/es/translation.json';
+import ca from './locales/ca/translation.json';
+//import * as RNLocalize from 'react-native-localize';
+
+const resources = {
+  en: { translation: en },
+  es: { translation: es },
+  ca: { translation: ca },
+};
+
+// Get device language
+//const locales = RNLocalize.getLocales();
+const languageTag = 'en';
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: languageTag,
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false, // react already escapes
+  },
+});
+
+export default i18n;
