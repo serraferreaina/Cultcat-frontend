6a7d81eb18f8877d6ae9457ab9fe708c5c4022b4
PathologicalMiss <toni.gratacos@estudiantat.upc.edu>
2025-10-11 17:56:07 +0200

    refactor(tabs): enhance tab layout with icon support and styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,39 @@
-import { Tabs, Redirect } from 'expo-router'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
 import { useTranslation } from 'react-i18next'
-//import { useAuth } from '../../hooks/useAuth';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
 
 export default function TabsLayout() {
-  //const { user } = useAuth();
-  //if (!user) return <Redirect href="/(auth)/login" />;
-
   const { t } = useTranslation()
 
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" options={{ title: t('home') }} />
-      <Tabs.Screen name="profile" options={{ title: t('profile') }} />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#C86A2E', // ACCENT
+        tabBarInactiveTintColor: '#999',
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: t('home'),
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: t('profile'),
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="cat" color={color} size={size} />
+          ),
+        }}
+      />
     </Tabs>
   )
 }
